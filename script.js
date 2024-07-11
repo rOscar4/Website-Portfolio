7166c97d9a0046b38cc4bb14242841e76f5bafea
rOscar4 <o.rodriguezbu@gmail.com>
2024-07-10 17:27:08 -0700

    CSS and Javascript updates
    for style and navigation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Sticking navbar
 const navigationBar = document.querySelector(".navigationBar");
 const scrollWatcher = document.createElement("div");
 
@@ -7,5 +8,40 @@ navigationBar.before(scrollWatcher);
 const navObserver = new IntersectionObserver((entries) => {
     navigationBar.classList.toggle("sticking", !entries[0].isIntersecting)
 });
-
 navObserver.observe(scrollWatcher)
+
+
+//Clicking on navbar links to scroll to section
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const targetSection = document.getElementById(this.getAttribute('href').substring(1));
+      const offset = targetSection.offsetTop;
+      
+      const navBar = document.getElementById("navigationBar");
+      const navBarHeight = navBar.offsetHeight;
+      const adjustedOffset = offset - navBarHeight - 30;
+
+      window.scrollTo({
+        top: adjustedOffset,
+        behavior: "smooth"
+        });
+    });
+});
+
+//contactButton click
+const contactButton = document.querySelector(".contactButton");
+const contactSection = document.getElementById("contact");
+const navBar = document.getElementById("navigationBar");
+
+const contactHeight = contactSection.offsetTop;
+const adjustedContact = contactHeight - (navBar.offsetHeight) - 30;
+
+contactButton.onclick = function() {
+    window.scrollTo({
+        top: adjustedContact,
+        behavior: "smooth"
+        });
+}
